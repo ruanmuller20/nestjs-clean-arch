b85aceedb6d1d2281230aa14baa300c71182974f
ruanmuller20 <ruanmuller20@gmail.com>
2025-07-16 09:57:13 -0300

    Adding Update methods

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -15,8 +15,20 @@ export class UserEntity extends Entity<UserProps> {
 
   }
 
+  update(value: string): void{
+    this.name = value;
+  }
+
+   updatePassword(value: string): void{
+    this.password = value;
+  }
+
   get name(){
     return this.props.name
+  }
+
+  private set name(value: string){
+    this.props.name = value;
   }
 
   get email(){
@@ -25,6 +37,10 @@ export class UserEntity extends Entity<UserProps> {
 
   get password(){
     return this.props.password
+  }
+
+  private set password(value: string){
+    this.props.password = value;
   }
 
   get createdAt(){
