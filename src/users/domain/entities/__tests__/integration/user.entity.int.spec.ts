d04b5e3b02ba7fa7a5eadb386fa33963098cd6ea
ruanmuller20 <ruanmuller20@gmail.com>
2025-07-25 09:45:36 -0300

    Integration tests - validate email field on create user

--- src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
@@ -22,7 +22,41 @@ describe("UserEntity Integration Tests", () => {
 
       props = {
         ...UserDataBuilder({}),
+        name: 10 as any,
+      }
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
         name: "a".repeat(256),
+      }
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+    });
+
+
+     it("Should throw an error when creating a user with invalid email", () =>{
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        email: null,
+      }
+
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        email: "",
+      }
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        email: 10 as any,
+      }
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        email: "a".repeat(256),
       }
       expect(() => new UserEntity(props)).toThrow(EntityValidationError);
     });
